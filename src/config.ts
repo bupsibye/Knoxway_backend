bbdc6cf1df57fd547c03886a141eac0f63db902c
bupsibye <wekcnupt@gmail.com>
2025-12-15 12:54:26 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = Number(process.env.PORT) || 4000;
-export const BOT_TOKEN = process.env.BOT_TOKEN || '';
-export const WEBHOOK_URL = process.env.WEBHOOK_URL || '';
+export const BOT_TOKEN = process.env.BOT_TOKEN || '8212274685:AAEN_jjb3hUnVN9CxdR9lSrG416yQXmk4Tk';
+export const WEBHOOK_URL = process.env.WEBHOOK_URL || 'https://knoxway-backend.onrender.com/telegram/webhook';
 
 if (!BOT_TOKEN) {
   console.warn('⚠️ BOT_TOKEN is not set in .env');
