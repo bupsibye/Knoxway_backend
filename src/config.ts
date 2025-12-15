afe4584f3bbf7cf5758b5504435157d14bf58dc3
bupsibye <wekcnupt@gmail.com>
2025-12-15 15:57:13 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const BOT_TOKEN = process.env.BOT_TOKEN || '';
 export const WEBHOOK_URL = process.env.WEBHOOK_URL || '';
 
 export const FRONTEND_URL =
-  process.env.FRONTEND_URL || 'https://knoxway-frontend.vercel.app';
+  process.env.FRONTEND_URL || 'https://knoxway-frontend.vercel.app/';
 
 export const BACKEND_URL =
   process.env.BACKEND_URL || 'https://knoxway-backend.onrender.com';
