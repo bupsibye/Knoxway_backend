aff4338ad2752f5725a98181281d90f1e1fde6f6
bupsibye <wekcnupt@gmail.com>
2025-12-15 13:14:12 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
-// backend/src/config.ts
+export const PORT = Number(process.env.PORT) || 3000;
 
 export const BOT_TOKEN = process.env.BOT_TOKEN || '';
 export const WEBHOOK_URL = process.env.WEBHOOK_URL || '';
+
 export const FRONTEND_URL =
   process.env.FRONTEND_URL || 'https://knoxway-frontend.vercel.app';
 
